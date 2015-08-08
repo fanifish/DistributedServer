3797a8baeb6c7bb353071929a8129dd5f9a35f54
fanifanu@gmail.com <fanifanu@gmail.com>
2015-08-08 10:59:49 -0500

    changing structure

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # testing script for the library distributed server system
 
-java Server < server.in > server_log &
+java ../build/classes/Server < server.in > server_log &
 s_pid=$!
 #java Server < server2.in &
 #s2_pid=$!
-java Client < c1.in > c1.out &
+java ../build/classes/Client < c1.in > c1.out &
 c1_pid=$!
-java Client < c2.in > c2.out &
+java ../build/classes/Client < c2.in > c2.out &
 c2_pid=$!
 sleep 5s
 kill -9 $s_pid
